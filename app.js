92806da588284f60e803d3f0adbd5f4b3adae451
danielduarte-hmn <135180855+danielduarte-hmn@users.noreply.github.com>
2023-06-02 21:31:14 -0300

    console log indicando modificacoes do daniel

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O daniel fez esse commit sem voce ver na aula!")
